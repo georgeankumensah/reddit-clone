e04faabc86d1e1dfd2c9805da2680fc89fe7dfe5
georgeankumensah <georgeankumensah@gmail.com>
2023-10-26 22:23:55 +0000

    created google auth providers

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:reddit_clone/features/auth/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:reddit_clone/theme/palette.dart';
+
+void main() async {
+// ...
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Reddit clone",
+      theme: Pallete.darkModeAppTheme,
+      home: const LoginScreen(),
+    );
+  }
+}
